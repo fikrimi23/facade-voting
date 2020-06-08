2656879de2fc4319987afdfc54cc5965be9c67e6
Fikri Muhammad Iqbal <fikri.miqbal23@gmail.com>
2020-06-09 03:11:28 +0700

    Add stub for feature user

---
+++ Controllers/FeatureVoteController.cs
@@ -0,0 +1,53 @@
+using System.Linq;
+using System.Threading.Tasks;
+using App.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Controllers
+{
+    public class FeatureVoteController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FeatureVoteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var features = from f in _context.Feature select f;
+
+            return View(await features
+                .Include(m => m.Category)
+                .OrderBy(m => m.VoteCount)
+                .ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var feature = await _context.Feature
+                .Include(m => m.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (feature == null) return NotFound();
+
+            return View(feature);
+        }
+
+        [Authorize]
+        public IActionResult Upvote(int? id)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        [Authorize]
+        public IActionResult Downvote(int? id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
